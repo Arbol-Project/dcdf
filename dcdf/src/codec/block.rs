7ab3ae232dd99934140687741208f4566df93040
Chris Rossi <chris@christophermrossi.com>
2022-06-21 15:00:41 -0400

    fix: test an edge case
    Also improve comments for block.rs.

--- dcdf/src/codec/block.rs
+++ dcdf/src/codec/block.rs
@@ -26,14 +26,24 @@ impl<I> Block<I>
 where
     I: PrimInt + Debug,
 {
-    /// Snapshot of first time instant
+    /// Construct a Block from a Snapshot and a series of Logs based on the Snapshot
+    ///
     pub fn new(snapshot: Snapshot<I>, logs: Vec<Log<I>>) -> Self {
+        if logs.len() > 254 {
+            panic!(
+                "Too many logs in one block. Maximum is 254. {} passed.",
+                logs.len()
+            );
+        }
+
         Self {
             snapshot: snapshot,
             logs: logs,
         }
     }
 
+    /// Get the cell value at the given time instant, row, and column
+    ///
     pub fn get(&self, instant: usize, row: usize, col: usize) -> I
     where
         I: PrimInt + Debug,
@@ -44,6 +54,14 @@ where
         }
     }
 
+    /// Get a subarray of the given instant
+    ///
+    /// This will allocate a new `ndarray::Array2` to hold the subarray. This is called by
+    /// `chunk::Chunk.iter_window` to iterate over time instants one at a time. To retrieve all
+    /// time instants of interest at once, it is more efficient to preallocate an `ndarray::Array3`
+    /// and call `block.fill_window` for each instant of interest, which is the strategy used by
+    /// `chunk::Chunk.get_window`.
+    ///
     pub fn get_window(
         &self,
         instant: usize,
@@ -65,6 +83,13 @@ where
         window
     }
 
+    /// Retrieve a subarray of the given instant and write it to a preallocated array.
+    ///
+    /// The array to write to isn't passed in explicitly. Instead a setter function, `set`, is
+    /// passed to write the value. This indirection provides a means of injecting data conversion
+    /// from the stored i64 to whatever the destination data type is. This is used, for instance,
+    /// to convert from fixed point to floating point representation for floating point datasets.
+    ///
     pub fn fill_window<S>(
         &self,
         set: S,
@@ -82,6 +107,10 @@ where
         }
     }
 
+    /// Search within a window for cells with values in a given range.
+    ///
+    /// Returns a vector of (row, col) pairs.
+    ///
     pub fn search_window(
         &self,
         instant: usize,
@@ -111,6 +140,8 @@ where
         }
     }
 
+    /// Write a block to a stream
+    ///
     pub fn serialize(&self, stream: &mut impl Write) -> io::Result<()> {
         stream.write_byte((self.logs.len() + 1) as u8)?;
         self.snapshot.serialize(stream)?;
@@ -120,6 +151,8 @@ where
         Ok(())
     }
 
+    /// Read a block from a stream
+    ///
     pub fn deserialize(stream: &mut impl Read) -> io::Result<Self> {
         let n_instants = stream.read_byte()? as usize;
         let snapshot = Snapshot::deserialize(stream)?;
@@ -132,8 +165,11 @@ where
         Ok(Self { snapshot, logs })
     }
 
+    /// Return the number of bytes in the serialized representation of the block
     pub fn size(&self) -> u64 {
-        1 + self.snapshot.size() + self.logs.iter().map(|l| l.size()).sum::<u64>()
+        1 // number of instants
+        + self.snapshot.size()
+        + self.logs.iter().map(|l| l.size()).sum::<u64>()
     }
 }
 
@@ -143,6 +179,8 @@ mod tests {
     use super::*;
     use ndarray::{arr3, s, Array3};
     use std::collections::HashSet;
+    use std::io::Seek;
+    use tempfile::tempfile;
 
     fn array8() -> Array3<i32> {
         arr3(&[
@@ -177,6 +215,44 @@ mod tests {
                 [4, 4, 4, 4, 5, 4, 4, 4],
             ],
         ])
+    }
+
+    fn too_many_logs() -> io::Result<()> {
+        let data = array8();
+        let snapshot = data.slice(s![0, .., ..]);
+        let logs = vec![
+            data.slice(s![1, .., ..]),
+            data.slice(s![2, .., ..]),
+            data.slice(s![0, .., ..]),
+        ];
+        let logs = logs.into_iter().cycle().take(300);
+
+        // This statement should panic, as the max number of logs for one block is 254
+        let block: Block<i32> = Block::new(
+            Snapshot::from_array(snapshot, 2),
+            logs.into_iter()
+                .map(|log| Log::from_arrays(snapshot, log, 2))
+                .collect(),
+        );
+
+        let mut file = tempfile()?;
+        block.serialize(&mut file)?;
+        file.sync_all()?;
+        file.rewind()?;
+
+        // The number of time instants in the block is written as a single byte, so the maximum
+        // number lof logs we can actually store in this block is 254. If we're allowed to actually
+        // create the block, then serializing and deserializing will lead to incorrect behavior.
+        let block: Block<i32> = Block::deserialize(&mut file)?;
+        assert_eq!(block.logs.len(), 300);
+
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_too_many_logs() {
+        too_many_logs().expect("should panic");
     }
 
     #[test]
